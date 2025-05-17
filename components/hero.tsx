63172dc2f33c5993d256a54937c2441244699a91
Ibrahim Chikani <147825977+IbrahimDev00@users.noreply.github.com>
2025-05-17 22:34:37 +0530

    Initial commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,7 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 
 export default function Header() {
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
-      </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    </div>
+    <FlickeringGrid squareSize={3} gridGap={5} flickerChance={0.45} color="rgb(46, 160, 67)" className="w-full h-1/2"/>
   );
 }
